c99f119a19839dbd4056293b8379cf0d1dc91d2e
Beanieboy75 <noahdabog1@gmail.com>
2025-09-22 15:03:10 +0000

    Finished Walker project

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
   // Your code goes here
 
+function makeDot(top, left, _elementID) {
+    $("<div>")
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", "black")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .appendTo(_elementID);
+  }
+  function rollDie(){
+    $("#die").empty();
+    const randomNum = Math.ceil(Math.random() * 6);
+    console.log("Rolled:", randomNum);
+    if (randomNum === 1) {
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, "#die"); // middle middle
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+}
+
+  else if (randomNum === 6) {
+      makeDot(25, 25, "#die"); // Top left
+      makeDot(25, 75, "#die"); // Top right
+      makeDot(50, 25, "#die"); // Middle left
+      makeDot(50, 75, "#die"); // Middle right
+      makeDot(75, 25, "#die"); // Bottom left
+      makeDot(75, 75, "#die"); // Bottom right
+    }
+  }
+  $("#die").on("click", rollDie);
 });
--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,64 +1,200 @@
 /* global $, sessionStorage */
 
-$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+$(document).ready(runProgram);
 
-  // Constant Variables
-  var FRAME_RATE = 60;
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+function runProgram() {
+    ////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////// SETUP /////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
 
-
-  // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-
-  /* 
-  This section is where you set up event listeners for user input.
-  For example, if you wanted to handle a click event on the document, you would replace 'eventType' with 'click', and if you wanted to execute a function named 'handleClick', you would replace 'handleEvent' with 'handleClick'.
-
-  Note: You can have multiple event listeners for different types of events.
-  */
-  $(document).on('eventType', handleEvent);                          
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-
-  /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
-  */
-  function newFrame() {
     
+    var FRAME_RATE = 60;
+    var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+    const MOVEMENT_SPEED = 5; 
+    
+    
+    const WALKER_WIDTH = 50;
+    const WALKER_HEIGHT = 50;
 
-  }
+    
+    const player1 = {
+        id: "#walker", 
+        x: 100,
+        y: 100,
+        xSpeed: 0,
+        ySpeed: 0,
+        width: WALKER_WIDTH,
+        height: WALKER_HEIGHT
+    };
+
+    
+    const player2 = {
+        id: "#walker2", 
+        x: 400,
+        y: 100,
+        xSpeed: 0,
+        ySpeed: 0,
+        width: WALKER_WIDTH,
+        height: WALKER_HEIGHT
+    };
+
+    
+    const KEY = {
+        ENTER: 13,
+       
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        
+        W: 87,
+        A: 65,
+        S: 83,
+        D: 68,
+    };
+
+   
+    var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);
+
+    
+    $(document).on("keydown", handleKeyDown);
+    $(document).on("keyup", handleKeyUp);
+    
   
-  /* 
-  This section is where you set up the event handlers for user input.
-  For example, if you wanted to make an event handler for a click event, you should rename this function to 'handleClick', then write the code that should execute when the click event occurs.
-  
-  Note: You can have multiple event handlers for different types of events.
-  */
-  function handleEvent(event) {
+    $(player1.id).on("click", function() {
+        changeColor(player1);
+    });
+    $(player2.id).on("click", function() {
+        changeColor(player2);
+    });
 
-  }
+    ////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
 
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+    
+    function newFrame() {
+        
+        repositionGameItem(player1);
+        wallCollision(player1);
+        redrawGameItem(player1);
 
-  
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
+       
+        repositionGameItem(player2);
+        wallCollision(player2);
+        redrawGameItem(player2);
+    }
 
-    // turn off event handlers
-    $(document).off();
-  }
-  
+   
+   function handleKeyDown(event) {
+
+       // Player 1 (Arrow Keys)
+        if (event.which === KEY.UP) {
+        player1.ySpeed = -MOVEMENT_SPEED;
+       } else if (event.which === KEY.DOWN) {
+       player1.ySpeed = MOVEMENT_SPEED;
+      }
+
+       if (event.which === KEY.LEFT) {
+         player1.xSpeed = -MOVEMENT_SPEED;
+       } else if (event.which === KEY.RIGHT) {
+         player1.xSpeed = MOVEMENT_SPEED;
+       }
+
+      
+         if (event.which === KEY.W) {
+         player2.ySpeed = -MOVEMENT_SPEED;
+         } else if (event.which === KEY.S) {
+         player2.ySpeed = MOVEMENT_SPEED;
+       }
+
+       
+         if (event.which === KEY.A) { 
+         player2.xSpeed = -MOVEMENT_SPEED;
+         } else if (event.which === KEY.D) {
+          player2.xSpeed = MOVEMENT_SPEED;  
+         }
+     }
+
+    
+    function handleKeyUp(event) {
+        
+        if (event.which === KEY.UP || event.which === KEY.DOWN) {
+            player1.ySpeed = 0;
+        }
+        if (event.which === KEY.LEFT || event.which === KEY.RIGHT) {
+            player1.xSpeed = 0;
+        }
+
+        
+        if (event.which === KEY.W || event.which === KEY.S) {
+            player2.ySpeed = 0;
+        }
+        if (event.which === KEY.A || event.which === KEY.D) {
+            player2.xSpeed = 0;
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function repositionGameItem(gameItem) {
+        gameItem.x += gameItem.xSpeed;
+        gameItem.y += gameItem.ySpeed;
+    }
+
+    
+    function wallCollision(gameItem) {
+        const boardWidth = $("#board").width();
+        const boardHeight = $("#board").height();
+
+      
+        if (gameItem.x < 0) {
+            gameItem.x -= gameItem.xSpeed;
+        }
+
+        
+        if (gameItem.x + gameItem.width > boardWidth) {
+            gameItem.x -= gameItem.xSpeed;
+        }
+
+       
+        if (gameItem.y < 0) {
+            gameItem.y -= gameItem.ySpeed;
+        }
+
+        
+        if (gameItem.y + gameItem.height > boardHeight) {
+            gameItem.y -= gameItem.ySpeed;
+        }
+    }
+
+
+    function redrawGameItem(gameItem) {
+        $(gameItem.id).css({
+            "left": gameItem.x + 'px',
+            "top": gameItem.y + 'px',
+        });
+    }
+
+    
+    function getRandomColor() {
+        return "#000000".replace(/0/g, function () {
+            return (~~(Math.random() * 16)).toString(16);
+        });
+    }
+
+    
+    function changeColor(gameItem) {
+        const randomColor = getRandomColor();
+        $(gameItem.id).css("background-color", randomColor);
+    }
+
+    
+    function endGame() {
+        clearInterval(interval);
+        $(document).off();
+    }
 }
